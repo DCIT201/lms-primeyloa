a01ebcf8428a4a065d0d206daca3a19a13d28495
Amoako Benedict Acheampong <160603063+primeyloa@users.noreply.github.com>
2024-12-12 16:53:11 +0000

    Update Book.java

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,12 @@
 public class Book {
+    //Initilalizing the necessary variables
+    private String bookTitle;
+    private String bookGenre;
+    private String bookAuthor;
+    private boolean bookAvailability;
 
+    // initializing the constructor for the book class
+    public Book(String bookT
     // This is your working class.
     
 }
