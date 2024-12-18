cd053f5ab05d0b7b17b031b4328ef0d01957b4ea
Amoako Benedict Acheampong <160603063+primeyloa@users.noreply.github.com>
2024-12-18 17:45:03 +0000

    Update Book.java

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,41 +1,69 @@
- public class Book {
-        private String bookId;
-        private String title;
-        private boolean isAvailable;
-        private String yearPublished;
-        private String authorName 
+public class Book {
+    private String bookId;
+    private String title;
+    private boolean isAvailable;
+    private String yearPublished;
+    private String author;
 
-        // Constructor
-        public Book(String bookId, String title) {
-            this.bookId = bookId;
-            this.title = title;
-            this.isAvailable = true;
-        }
+    // Constructor
+    public Book(String bookId, String title, String yearPublished, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.isAvailable = true;
+    }
 
-        // Getters
-        public String getBookId() {
-            return bookId;
-        }
+    public Book(String bookId, String title) {
+    }
 
-        public String getTitle() {
-            return title;
-        }
+    // Getters
+    public String getBookId() {
+        return bookId;
+    }
 
-        public boolean isAvailable() {
-            return isAvailable;
-        }
+    public String getTitle() {
+        return title;
+    }
 
-        // Borrow book
-        public void borrowBook() {
-            if (isAvailable) {
-                isAvailable = false;
-            } else {
-                System.out.println("Book is already borrowed.");
-            }
-        }
+    public String getAuthor() {
+        return author;
+    }
 
-        // Return book
-        public void returnBook() {
-            isAvailable = true;
+    public void setAuthor(){
+        this.author = author;
+    }
+
+    public String getYearPublished() {
+        return yearPublished;
+    }
+    public void setYearPublished(){
+        this.yearPublished = yearPublished;
+    }
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    // Borrow book
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+        } else {
+            System.out.println("Book is already borrowed.");
         }
     }
+
+    // Return book
+    public void returnBook() {
+        isAvailable = true;
+    }
+}
